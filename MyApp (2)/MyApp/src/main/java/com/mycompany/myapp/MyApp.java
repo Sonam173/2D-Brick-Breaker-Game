3bb43dea38ebc862b9eaf8d3a19f8b17ddfbd329
Sonam Sahni <115079315+Sonam173@users.noreply.github.com>
2023-04-21 00:14:04 +0530

    Add files via upload

---
+++ MyApp (2)/MyApp/src/main/java/com/mycompany/myapp/MyApp.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myapp;
+
+
+
+/**
+ *
+ * @author Hp
+ */
+import javax.swing.JFrame;
+public class MyApp {
+
+    public static void main(String[] args) {
+       // System.out.println("Hello World!");
+        JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("BrickBreaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+}
